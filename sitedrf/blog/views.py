3289d9150407e4d1384f59ea9e6e22be3b422ce5
Alex <soyvitaly@gmail.com>
2022-05-18 10:21:21 +0200

    Add pagination

--- sitedrf/blog/views.py
+++ sitedrf/blog/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django.shortcuts import render
 
@@ -12,10 +13,17 @@ from .models import Category, Women
 from .serializer import WomenSerializer
 
 
+class WomenSetPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 10
+
+
 class WomenListView(generics.ListCreateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = (WomenSetPagination,)
 
 
 class WomenUpdateView(generics.RetrieveUpdateDestroyAPIView):
